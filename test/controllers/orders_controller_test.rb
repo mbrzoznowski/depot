4fd7ee0c9c5e17d3fbc9370acc223f70e50fcfe4
Michal Brzoznowski <mbrzoznowski@mbp-michal.home>
2017-09-07 14:36:29 +0200

    Checkout

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -16,4 +16,12 @@ class OrdersControllerTest < ActionController::TestCase
     get :new
     assert_response :success   
   end
+
+  test 'should create order' do
+    assert_difference('Order.count') do
+      post :create, order: { address: @order.address, email: @order.email, 
+            name: @order.name, pay_type: @order.pay_type}
+    end
+    assert_redirected_to store_path
+  end
 end
